864da4992466997a25580b4130626b61e470db9f
Evgeniy Guskov <evgeniy.guskov.code@gmail.com>
2020-07-13 02:30:36 +0500

    Minor changes and code improvement

--- main.js
+++ main.js
@@ -1,48 +1,40 @@
 $(document).ready(function () {
-
     const $form = $('#form');
     const $text = $('#text');
     const $todoList = $('#todoList');
 
+    const delete_button = `<button class="delete-button">&#10060;</button>`;
+    
     let todo = [];
 
-    const render = (arr) => {                //Отрисовывает ненумерованный список
+    const render = (arr) => {
         let content = '';
-
         arr.forEach(item => {
-            content += `<li id="${item.id}"><span>${item.text}</span><button class="delete-button">&#10060;</li>`
+            content += `<li id="${item.id}"><span>${item.text}</span>${delete_button}</li>`
         });
-
         $todoList.html(content);
     }
 
-    const onFormSubmit = () => {          //Наполняет массив, назначает свойства его элементам; рисует список
+    const onFormSubmit = () => {
         todo.push({
             id: Math.random(),
             status: false,
             text: $text.val(),
         });
-
         $text.val('');
-
         render(todo);
     }
 
-    $form.on('submit', function (event) {   //обработчик события отправки формы
+    $form.on('submit', function (event) {
         event.preventDefault();
-
         onFormSubmit();
     });
 
-    $todoList.on('click', '.delete-button', function (event) {   //делегированный обработчик события клика на кнопку крестика
-        const idChecker = (item => {
-            return item.id == $(this).parent().attr('id')
-        });
-
-        const arrayIndex = todo.findIndex(idChecker);
-
-        todo.splice(arrayIndex, 1);
-
-        render(todo);
+    $todoList.on('click', '.delete-button', function () {
+        const arrayIndex = todo.findIndex(item => String(item.id) === $(this).parent().attr('id'));
+        if (arrayIndex !== -1) {
+            todo.splice(arrayIndex, 1);
+            render(todo);
+        }
     });
 })
